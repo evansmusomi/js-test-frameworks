57d929153e58a0c2b891ca3f80af53f630fb769e
Evans Musomi <evans.musomi@gmail.com>
2017-08-21 17:03:32 +0300

    Add jasminen tests and implementation for simplistic subtract, multiply and divide functions

--- Math/Math.js
+++ Math/Math.js
@@ -3,6 +3,15 @@
 const Math = {
     add(first, second){
         return first + second;
+    },
+    subtract(first, second){
+        return first - second;
+    },
+    multiply(first, second){
+        return first * second;
+    },
+    divide(first, second){
+        return first / second;
     }
 }
 
--- spec/Math.spec.js
+++ spec/Math.spec.js
@@ -15,4 +15,19 @@ describe("Math", function(){
         let result = math.add(first, second);
         expect(result).toEqual(first+second)
     });
+
+    it("should subtract two numbers", function(){
+        let result = math.subtract(first, second);
+        expect(result).toEqual(first-second)
+    });
+
+    it("should multiply two numbers", function(){
+        let result = math.multiply(first, second);
+        expect(result).toEqual(first*second)
+    });
+
+    it("should divide two numbers", function(){
+        let result = math.divide(first, second);
+        expect(result).toEqual(first/second);
+    });
 });
